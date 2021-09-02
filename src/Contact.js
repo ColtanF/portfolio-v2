160487676206e57abc25df23a5ad66195f3f7bd2
ColtanF <coltan.franke@gmail.com>
2021-09-02 14:22:44 -0500

    Added social links and styling

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, Form } from "semantic-ui-react";
+import { SiLinkedin, SiGithub } from "react-icons/si";
 
 const Contact = () => {
+  const iconSize = 30;
   return (
     <section className="ui segment vertical container">
       <h2>Contact me</h2>
@@ -10,6 +12,22 @@ const Contact = () => {
           <p>
             Get in touch here, or send me an email at coltan.franke@gmail.com.
           </p>
+          <div className="ui relaxed stackable grid">
+            <div className="one wide middle aligned column">
+              <SiLinkedin size={iconSize} />
+            </div>
+            <div className="fourteen wide middle aligned column link">
+              <a href="https://www.linkedin.com/in/coltan-franke-124515177/">
+                LinkedIn
+              </a>
+            </div>
+            <div className="one wide middle aligned column">
+              <SiGithub size={iconSize} />
+            </div>
+            <div className="eight wide middle aligned column link">
+              <a href="https://github.com/ColtanF">GitHub</a>
+            </div>
+          </div>
         </div>
         <div className="eight wide column">
           <ContactForm />
